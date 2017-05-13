2c3406aecc5eea0263763c8fe74a8dacb266d550
simonseyock <simonseyock@googlemail.com>
2017-05-13 13:04:35 +0200

    removed duplicate comment

--- Übungsblatt3/Aufgabe1/src/Sheep.java
+++ Übungsblatt3/Aufgabe1/src/Sheep.java
@@ -1,20 +1,3 @@
-// interface Cloneable:
-// Implementierung:
-// http://beginnersbook.com/2015/01/cloneable-interface-in-java-object-cloning/
-// interview with a developer of java core features about the interface Cloneable:
-// http://www.artima.com/intv/bloch13.html
-
-// Warum Cloneable nicht empfohlen wird:
-// 1. Das Interface ist irrefuehrend, da es keine Methode clone enthaelt.
-// Normalerweise dienen Interfaces dazu, Objekte gleich behandeln zu koennen.
-// Dies wird durch dieses Interface nicht ermoeglicht.
-// 2. Es erzwingt das Behandlen der CloneNotSupported Exception.
-// 3. Es erlaubt Zugriff auf die Methode Object.clone, die einen shallow clone des
-// Objektes erzeugt. Dies wird als unsauber angesehen, da dies ein Weg ist ausserhalb
-// der Syntax der Sprache ein neues Objekt zur erstellen ('It's based on field copies,
-// and it's "extra-linguistic." It creates an object without calling a constructor.',
-// Josh Bloch, http://www.artima.com/intv/bloch13.html)
-
 public class Sheep {
 	public String name;
 	public Fur fur;
