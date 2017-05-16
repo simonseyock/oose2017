8a214fcae9812fffc10564cca41a3156c72e9b95
unknown <schroederl@AMSEL.informatik.uni-bonn.de>
2017-05-16 13:01:26 +0200

    Aufgabe 3

---
+++ Uebungsblatt4/Aufgabe3/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+	public static void main(String[] args) {
+		 Food food;
+		 food = new Cauliflower();
+		 System.out.println(food.getMeal());
+		 
+		 // Variable food wird mit einem Element aus Klasse Spaghetti ueberschrieben
+		 // Das geht, da die Variable food vom Typ Food ist und Spaghetti ein
+		 // von Food abgeleiteter Typ ist.
+		 food = new Spaghetti(); 
+		 System.out.println(food.getMeal());
+		 food = new Pizza();
+		 System.out.println(food.getMeal());
+		 
+		 Student student = new Student();
+		 
+		 Cauliflower essen1 = new Cauliflower();
+		 student.eat(essen1);
+
+		 Spaghetti essen2 = new Spaghetti();
+		 student.eat(essen2);
+
+		 Food essen = new Cauliflower();
+		 student.eat(essen);
+	}
+}
