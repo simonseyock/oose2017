69bd1bc07b99af5a2b270c45148013797967dc38
simonseyock <simonseyock@googlemail.com>
2017-05-20 14:35:29 +0200

    added explanation

--- Uebungsblatt4/Aufgabe3/src/Main.java
+++ Uebungsblatt4/Aufgabe3/src/Main.java
@@ -15,12 +15,21 @@ public class Main {
 		 
 		 Student student = new Student();
 		 
+		 // Da die Klasse Student mehrere Methoden mit dem Name eat hat,
+		 // wird anhand der statischen Typen immer die spezifischste Methodensignatur ausgewaehlt.
+		 // D.h. Zuerst wird ueberprueft, ob eine Signatur mit dem statischen Typen der uebergebenen
+		 // Variable uebereinstimmt, wenn nicht, dann wird ueberprueft ob eine Signatur mit dem Typen
+		 // der direkten Elternklasse uebereinstimmt, dann die Elternklasse davon usw.
+		 
+		 // statischer Typ Cauliflower, spezifischste Signatur ist void eat(Cauliflower)
 		 Cauliflower essen1 = new Cauliflower();
 		 student.eat(essen1);
 
+		 // statischer Typ Spaghetti, spezifischste Signatur ist void eat(Food)
 		 Spaghetti essen2 = new Spaghetti();
 		 student.eat(essen2);
 
+		 // statischer Typ Food, spezifischste Signatur ist void eat(Food)
 		 Food essen = new Cauliflower();
 		 student.eat(essen);
 	}
